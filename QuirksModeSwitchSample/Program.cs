723eeadabe6d043492f0ede6fcbaafc18134c3f4
Eilon Lipton <elipton@microsoft.com>
2017-04-06 16:26:22 -0700

    Initial commit

---
+++ QuirksModeSwitchSample/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Reflection;
+
+namespace QuirksModeSwitchSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(typeof(int).GetTypeInfo().Assembly.CodeBase);
+
+            //AppContext.SetSwitch("switch:aspnet.test", true);
+
+            var success = AppContext.TryGetSwitch("switch:aspnet.test", out var isEnabled);
+            Console.WriteLine("Switch found? " + success);
+            Console.WriteLine("Switch enabled? " + isEnabled);
+        }
+    }
+}
