8e331e55502d6f18000fb7ad8c77594bf3779f95
Eilon Lipton <elipton@microsoft.com>
2017-04-06 16:35:42 -0700

    Add comments, project cleanup

--- QuirksModeSwitchSample/Program.cs
+++ QuirksModeSwitchSample/Program.cs
@@ -7,10 +7,13 @@ namespace QuirksModeSwitchSample
     {
         static void Main(string[] args)
         {
+            // Show whether running on .NET Core or .NET Framework
             Console.WriteLine(typeof(int).GetTypeInfo().Assembly.CodeBase);
 
+            // Programmatically set the switch
             //AppContext.SetSwitch("switch:aspnet.test", true);
 
+            // Show switch value
             var success = AppContext.TryGetSwitch("switch:aspnet.test", out var isEnabled);
             Console.WriteLine("Switch found? " + success);
             Console.WriteLine("Switch enabled? " + isEnabled);
