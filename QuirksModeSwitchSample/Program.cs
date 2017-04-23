ecac2db100228f9829bf199d61f11c7254647e16
N. Taylor Mullen <nimullen@microsoft.com>
2017-04-23 11:48:08 -0700

    Add net451 support to sample. (#1)
    - Used configuration manager and ifdefs to control how switches are retrieved/set.

--- QuirksModeSwitchSample/Program.cs
+++ QuirksModeSwitchSample/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+#if NET451
+using System.Configuration;
+using System.Linq;
+#endif
 using System.Reflection;
 
 namespace QuirksModeSwitchSample
@@ -10,11 +14,20 @@ namespace QuirksModeSwitchSample
             // Show whether running on .NET Core or .NET Framework
             Console.WriteLine(typeof(int).GetTypeInfo().Assembly.CodeBase);
 
+#if NET451
+            // Programmatically set the switch
+            //ConfigurationManager.AppSettings.Set("switch:aspnet.test", "true");
+
+            var value = ConfigurationManager.AppSettings.GetValues("switch:aspnet.test")?.FirstOrDefault();
+            var success = bool.TryParse(value, out var isEnabled);
+#else
             // Programmatically set the switch
             //AppContext.SetSwitch("switch:aspnet.test", true);
+            
+            var success = AppContext.TryGetSwitch("switch:aspnet.test", out var isEnabled);
+#endif
 
             // Show switch value
-            var success = AppContext.TryGetSwitch("switch:aspnet.test", out var isEnabled);
             Console.WriteLine("Switch found? " + success);
             Console.WriteLine("Switch enabled? " + isEnabled);
         }
